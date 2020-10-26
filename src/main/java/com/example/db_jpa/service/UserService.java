0307067bd169fead0da2718fd6bc144dc1a84352
asraysun <58439394+asraysun@users.noreply.github.com>
2020-10-26 21:59:49 +0300

    Initial commit

---
+++ src/main/java/com/example/db_jpa/service/UserService.java
@@ -0,0 +1,31 @@
+package com.example.db_jpa.service;
+
+import com.example.db_jpa.model.User;
+import com.example.db_jpa.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User findById(Long id) {
+        return userRepository.getOne(id);
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User saveUser(User user) {
+            return userRepository.save(user);
+    }
+
+    public void deleteById(Long id) {
+        userRepository.deleteById(id);
+    }
+}
